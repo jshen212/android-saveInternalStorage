c70ff0b9ca71b036fdb3465da5577fbe75d73d6a
simplejeff <jeffshen86@gmail.com>
2016-10-02 17:24:33 -0700

    [feature] successfully made app that saves data to internal storage

---
+++ app/src/main/java/mydiary/androidstudio/com/mydiary/MainActivity.java
@@ -0,0 +1,92 @@
+package mydiary.androidstudio.com.mydiary;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText enterText;
+    private Button saveButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        enterText = (EditText) findViewById(R.id.editText);
+        saveButton = (Button) findViewById(R.id.saveButton);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!enterText.getText().toString().equals("")) {
+
+                    String data = enterText.getText().toString();
+
+                    writeToFile(data);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please enter text", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        if(readFromFile() != null) {
+            enterText.setText(readFromFile());
+        }
+    }
+
+    private void writeToFile(String myData) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput("diary.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(myData);
+            outputStreamWriter.close();
+
+
+
+        } catch(IOException e) {
+            Log.v("MyActivity", e.toString());
+        }
+    }
+
+    private String readFromFile() {
+        String result = "";
+
+        try {
+
+            InputStream inputStream = openFileInput("diary.txt");
+
+            if(inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                String tempString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while( (tempString = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(tempString);
+                }
+                inputStream.close();
+
+                result = stringBuilder.toString();
+            }
+
+        } catch(IOException e) {
+            Log.v("My Activity", "File not found "  + e.toString());
+        }
+
+        return result;
+    }
+
+}
